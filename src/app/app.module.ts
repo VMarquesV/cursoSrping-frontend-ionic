b6e3ce1159388db34c914edf10e9542b695e3a98
VMarquesV <viniciusmar2014@outlook.com>
2022-04-26 13:11:17 -0300

    Criando um  Interceptador para tratamento de erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CategoriaService } from '../services/domain/categoria.service'; 
-
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, CategoriaService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+  CategoriaService, ErrorInterceptorProvider ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/interceptors/error-interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor{
+ 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+        return next.handle(req)
+                .pipe(
+                    catchError(error => {
+                       if( !error.status ){
+                            error = JSON.parse(error);
+                        }
+ 
+                        return throwError(error);
+                    })) as any;
+    }
+}
+
+export const ErrorInterceptorProvider = {
+	provide: HTTP_INTERCEPTORS,
+	useClass: ErrorInterceptor,
+	multi: true,
+};
